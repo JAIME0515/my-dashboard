7c5168068747b3d3ec42a2a4a2db285e7651d71e
jaime.apodaca <jaime.apodaca@echtech.mx>
2025-03-13 16:36:34 -0700

    add store pokemons

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -1,4 +1,4 @@
-import { PokemonGrid } from "@/pokemons";
+import { FavoritePokemons } from "@/pokemons/components/FavoritePokemons";
 
 export const metadata = {
   title: "Favoritos",
@@ -8,11 +8,9 @@ export const metadata = {
 export default async function PokemonsPage() {
   return (
     <div className="flex flex-col">
-      <span className="text-5xl my-2">
-        Pokémons Favoritos <small className="text-blue-500">Global State</small>
-      </span>
+      <span className="text-5xl my-2">Pokémons Favoritos</span>
 
-      <PokemonGrid pokemons={[]} />
+      <FavoritePokemons />
     </div>
   );
 }
